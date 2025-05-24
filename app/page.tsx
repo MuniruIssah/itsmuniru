7a88358d51e28f7fc0b6fc55ff66f5059d4d9469
[Muniru Issah] <munirissah2092@gmail.com>
2025-05-24 14:50:52 +0000

    wip: first version of my updated portfolio

--- app/page.tsx
+++ app/page.tsx
@@ -1,103 +1,187 @@
 import Image from "next/image";
+import { Slackey } from 'next/font/google'
+import { SquareArrowOutUpRight } from "lucide-react";
+import { currentStack, experience, projects } from "@/data";
+import { Badge } from "@/components/ui/badge";
+import { Accordion, AccordionContent, AccordionItem, AccordionTrigger } from "@/components/ui/accordion";
+import { SiGithub, SiLinkedin } from "react-icons/si";
+
+const slackey = Slackey({
+  weight: '400',
+  subsets: ['latin'],
+})
+
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
+    <div className="grid grid-rows-[auto_1fr_auto] items-center max-w-4xl mx-auto min-h-screen px-4 py-6 gap-6 sm:px-8 sm:py-10 md:px-20 md:py-20 font-[family-name:var(--font-geist-sans)]">
+      <header className="row-start-1 flex flex-col sm:flex-row gap-4 sm:gap-6 items-start sm:items-center justify-between">
+        <div className="flex flex-wrap gap-4 sm:gap-6 items-center">
           <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+            className="flex items-center gap-2 hover:underline hover:underline-offset-4 text-sm sm:text-base"
+            href="/Issah-Muniru-Resume-2024.pdf"
             target="_blank"
             rel="noopener noreferrer"
           >
             <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
+              aria-hidden
+              src="/file.svg"
+              alt="File icon"
+              width={16}
+              height={16}
             />
-            Deploy now
+            Resume
           </a>
           <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+            className="flex items-center gap-2 hover:underline hover:underline-offset-4 text-sm sm:text-base"
+            href="https://github.com/MuniruIssah"
             target="_blank"
             rel="noopener noreferrer"
           >
-            Read our docs
+            <SiGithub className="text-muted-foreground"/>
+            Github
+          </a>
+          <a
+            className="flex items-center gap-2 hover:underline hover:underline-offset-4 text-sm sm:text-base"
+            href="https://www.linkedin.com/in/issah-muniru-b3715a206/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <SiLinkedin className="text-muted-foreground"/>
+            LinkedIn
           </a>
         </div>
+      </header>
+      <main className="flex flex-col gap-6 sm:gap-8 row-start-2 items-center sm:items-start">
+        <div className="flex flex-col gap-2">
+          <span className="font-mono text-secondary-foreground text-sm sm:text-base">Hi! I'm</span>
+          <h1 className="text-4xl sm:text-5xl md:text-7xl font-extrabold leading-tight" style={slackey.style}>Muniru Issah</h1>
+          <p className="font-mono text-secondary-foreground text-sm sm:text-base max-w-2xl">I'm a Fullstack Applications Developer with 6 years of experience building products in the healthcare and banking industries — most recently at ViuHealth.</p>
+        </div>
+
+        <div className="flex flex-col gap-4 w-full">
+          <h2 className="text-xl sm:text-2xl font-extrabold" style={slackey.style}>Current Stack</h2>
+          <p className="font-mono text-secondary-foreground text-sm sm:text-base">My tech stack adapts based on the specific needs of each project and the environment of the organization I&apos;m working with.</p>
+          <div className="grid grid-cols-1 sm:grid-cols-2 gap-6 mt-2">
+            <div className="flex flex-col gap-2">
+              <span className="text-muted-foreground font-mono text-xs sm:text-sm">Languages & Frameworks</span>
+              <div className="flex gap-2 flex-wrap mt-2">
+                {currentStack[0].items.map((item) => (
+                  <div key={item.label} className="flex items-center gap-2 border border-stone-400 rounded-full py-1 px-2 sm:py-1.5 sm:px-3 hover:bg-foreground hover:text-background transition-colors duration-300 cursor-pointer">
+                    {item.icon}
+                    <span className="text-xs sm:text-sm">{item.label}</span>
+                  </div>
+                ))}
+              </div>
+            </div>
+            <div className="flex flex-col gap-2">
+              <span className="text-muted-foreground font-mono text-xs sm:text-sm">Tools & Others</span>
+              <div className="flex gap-2 flex-wrap mt-2">
+                {currentStack[1].items.map((item) => (
+                  <div key={item.label} className="flex items-center gap-2 border border-stone-400 rounded-full py-1 px-2 sm:py-1.5 sm:px-3 hover:bg-foreground hover:text-background transition-colors duration-300 cursor-pointer">
+                    {item.icon}
+                    <span className="text-xs sm:text-sm">{item.label}</span>
+                  </div>
+                ))}
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className="flex flex-col gap-4 w-full mt-2">
+          <h2 className="text-xl sm:text-2xl font-extrabold" style={slackey.style}>Experience</h2>
+          <p className="font-mono text-secondary-foreground text-sm sm:text-base">Here's a brief overview of my professional journey.</p>
+          <div className="flex flex-col gap-6 w-full mt-2 divide-y divide-dashed divide-stone-400">
+            {experience.map((item, index) => (
+              <div key={index} className="flex flex-col gap-2 w-full pb-6">
+                <div className="flex flex-col sm:flex-row sm:justify-between sm:items-center w-full gap-1">
+                  <span className="text-muted-foreground font-mono text-xs sm:text-sm">{item.company} - {item.role}</span>
+                  <span className="text-muted-foreground font-mono text-xs sm:text-sm">{item.duration}</span>
+                </div>
+
+                <div className="flex flex-col gap-2 mt-2">
+                  <div className="flex gap-2 flex-wrap">
+                    {item.technologies.map((tech) => (
+                      <div key={tech.label} className="flex items-center cursor-pointer text-muted-foreground text-base sm:text-lg">
+                        {tech.icon}
+                      </div>
+                    ))}
+                  </div>
+
+                  <div className="flex flex-col gap-2 mt-2">
+                    <span className="text-muted-foreground font-mono text-xs sm:text-sm">My Role:</span>
+                    <span className="text-secondary-foreground font-mono text-sm sm:text-base">{item.description}</span>
+                  </div>
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
+
+        <div className="flex flex-col gap-4 w-full mt-2">
+          <h2 className="text-xl sm:text-2xl font-extrabold" style={slackey.style}>Projects</h2>
+          <p className="font-mono text-secondary-foreground text-sm sm:text-base">A lot of my work is not public so the code is not accessible. However, i'm working on a few projects that are open source and will be available soon.</p>
+
+          <div className="flex flex-col gap-4 w-full mt-2">
+            <span className="text-muted-foreground font-mono text-xs sm:text-sm">Currently working on:</span>
+            <div className="flex flex-col sm:flex-row sm:divide-x sm:divide-stone-400 gap-2 sm:gap-0 mt-1">
+              <a href="https://movanow.co"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="flex items-center gap-2 hover:underline hover:underline-offset-4 sm:pr-2 text-sm sm:text-base"
+              >Mova <SquareArrowOutUpRight size={14} className="text-muted-foreground" /></a>
+              <a href="https://mallyhealth.com"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="flex items-center gap-2 hover:underline hover:underline-offset-4 sm:px-2 text-sm sm:text-base "
+              >Mally Health <SquareArrowOutUpRight size={14} className="text-muted-foreground" /></a>
+              <a href="https://mallyhealth.com"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="flex items-center gap-2 hover:underline hover:underline-offset-4 text-sm sm:text-base sm:px-2"
+              >Pense Health <SquareArrowOutUpRight size={14} className="text-muted-foreground" /></a>
+            </div>
+            <span className="text-muted-foreground font-mono text-xs sm:text-sm block mt-3">Previously worked on:</span>
+
+            <div className="flex flex-col gap-6 w-full mt-2">
+              {projects.map((project, index) => (
+                <div key={index} className="flex flex-col gap-2 w-full">
+                  <Badge className="font-mono text-xs sm:text-sm">{project.company}</Badge>
+                  <div className="flex flex-col gap-2 p-2">
+                    <span className="text-foreground font-mono font-bold text-sm sm:text-base">{project.title}</span>
+                    <span className="text-secondary-foreground font-mono font-light text-sm sm:text-base">{project.description}</span>
+                    <Accordion type="single" collapsible className="w-full border border-muted-foreground rounded-md mt-2 px-3 text-sm sm:text-base">
+                      <AccordionItem value="item-1">
+                        <AccordionTrigger> 
+                          <div className="flex flex-col sm:flex-row sm:items-center justify-between gap-2 w-full">
+                            <span className="text-muted-foreground font-mono text-xs sm:text-sm">My Roles</span>
+                            <div className="flex items-center gap-2">
+                              {project.stack.map((stack) => (
+                                <div key={stack.label} className="flex items-center gap-2 text-base sm:text-lg">
+                                  {stack.icon}
+                                </div>
+                              ))}
+                            </div>
+                          </div>
+                        </AccordionTrigger>
+                        <AccordionContent>
+                          <ul className="flex flex-col gap-3 text-sm sm:text-base">
+                            {project.roles.map((role) => (
+                              <li key={role} className="text-secondary-foreground font-mono">
+                                {role}
+                              </li>
+                            ))}
+                          </ul>
+                        </AccordionContent>
+                      </AccordionItem>
+                    </Accordion>
+                  </div>
+                </div>
+              ))}
+            </div>
+          </div>
+        </div>
       </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
     </div>
   );
 }
---
+++ data/index.tsx
@@ -0,0 +1,232 @@
+import { SiAngular, SiAstro, SiDocker, SiDotnet, SiExpo, SiExpress, SiGit, SiGithub, SiMongodb, SiNextdotjs, SiPython, SiReact, SiReactrouter, SiRedis, SiSlack, SiSupabase, SiTailwindcss, SiTypescript } from "react-icons/si";
+import { FaAws, FaNodeJs } from "react-icons/fa6";
+import { TbBrandCSharp, TbBrandReactNative } from "react-icons/tb";
+import { VscAzure } from "react-icons/vsc";
+import { RiFirebaseFill } from "react-icons/ri";
+
+const currentStack = [
+  {
+    name: 'Languages & Frameworks',
+    items: [
+      { label: 'React', icon: <SiReact /> },
+      { label: 'React Native', icon: <TbBrandReactNative /> },
+      { label: 'Expo', icon: <SiExpo /> },
+      { label: 'Next.js', icon: <SiNextdotjs /> },
+      { label: 'Angular', icon: <SiAngular /> },
+      { label: 'Astro', icon: <SiAstro /> },
+      { label: 'React Router', icon: <SiReactrouter /> },
+      { label: 'Tailwind CSS', icon: <SiTailwindcss /> },
+      { label: 'TypeScript', icon: <SiTypescript /> },
+      { label: 'Node.js', icon: <FaNodeJs /> },
+      { label: 'Express', icon: <SiExpress /> },
+      { label: 'MongoDB', icon: <SiMongodb /> },
+      { label: 'Firebase', icon: <RiFirebaseFill /> },
+      { label: 'Supabase', icon: <SiSupabase /> },
+      { label: 'C#', icon: <TbBrandCSharp /> },
+      { label: '.NET', icon: <SiDotnet /> },
+
+    ]
+  },
+  {
+    name: 'Tools & Others',
+    items: [
+      { label: 'Git', icon: <SiGit /> },
+      { label: 'GitHub', icon: <SiGithub /> },
+      { label: 'Slack', icon: <SiSlack /> },
+      { label: 'Redis', icon: <SiRedis /> },
+      { label: 'Docker', icon: <SiDocker /> },
+      { label: 'AWS', icon: <FaAws /> },
+
+    ]
+  }
+]
+
+const experience = [
+  {
+    company: 'Viuhealth',
+    role: 'Senior Fullstack Developer',
+    duration: 'Aug 2023 - Jan 2025',
+    description: 'Collaborated with the team to restructure key applications for improved efficiency and scalability. Built internal component and icon libraries for reuse across projects. Contributed new features while supporting the mobile team with backend integration. Managed and maintained the company’s CI/CD pipeline.',
+    technologies: [
+      { label: 'Angular', icon: <SiAngular /> },
+      { label: 'Node.js', icon: <FaNodeJs /> },
+      { label: 'Express', icon: <SiExpress /> },
+      { label: 'MongoDB', icon: <SiMongodb /> },
+      { label: 'Firebase', icon: <RiFirebaseFill /> },
+      { label: 'AWS', icon: <FaAws /> }
+    ]
+  },
+  {
+    company: 'GTBank Ghana',
+    role: 'Applications Developer',
+    duration: 'Mar 2022 - Aug 2023',
+    description: 'As a utility developer, I was responsible for developing and maintaining over six applications — ranging from internal tools that streamlined operations to customer-facing platforms that generated revenue for the bank.',
+    technologies: [
+      { label: 'React', icon: <SiReact /> },
+      { label: 'React Native', icon: <TbBrandReactNative /> },
+      { label: 'Tailwind CSS', icon: <SiTailwindcss /> },
+      { label: 'TypeScript', icon: <SiTypescript /> },
+      { label: 'C#', icon: <TbBrandCSharp /> },
+      { label: '.NET', icon: <SiDotnet /> },
+      { label: 'Redis', icon: <SiRedis /> },
+      { label: 'Azure', icon: <VscAzure /> },
+    ]
+  },
+  {
+    company: 'OrigoHealth',
+    role: 'Frontend Developer',
+    duration: 'Sep 2021 - Mar 2023',
+    description: 'Collaborated with the team to develop and maintain the company’s dashboards and backend services, ensuring reliability and performance across core systems.',
+    technologies: [
+      { label: 'React', icon: <SiReact /> },
+      { label: 'Node.js', icon: <FaNodeJs /> },
+      { label: 'Express', icon: <SiExpress /> },
+      { label: 'MongoDB', icon: <SiMongodb /> },
+      { label: 'Docker', icon: <SiDocker /> },
+    ]
+  }
+]
+
+
+const projects = [
+  {
+    company: 'Viuhealth',
+    title: 'Viuhealth WebApps',
+    stack: [
+      { label: 'Angular', icon: <SiAngular /> },
+      { label: 'Node.js', icon: <FaNodeJs /> },
+      { label: 'Express', icon: <SiExpress /> },
+      { label: 'MongoDB', icon: <SiMongodb /> },
+      { label: 'Firebase', icon: <RiFirebaseFill /> },
+      { label: 'AWS', icon: <FaAws /> }
+    ],
+    description: 'A suite of web applications designed to support the management of patients with chronic conditions. This included a patient onboarding portal, an admin dashboard, a clinical trial site, and a sponsor portal.',
+    roles: [
+      "Consolidated all applications into a single monorepo to improve development efficiency and reduce maintenance overhead.",
+      "Configured CI/CD pipelines to automate builds and deployments, with workflows triggered by changes within each application's folder.",
+      "Developed a shared components and icon library to establish a consistent design system across all applications.",
+      "Conducted code reviews to maintain code quality, enforce standards, and ensure consistency across the team.",
+      "Acted as the development team representative in weekly product meetings, providing updates on application status and aligning on roadmap priorities."
+    ]
+  },
+  {
+    company: 'Viuhealth',
+    title: 'TrialViu',
+    stack: [
+      { label: 'Angular', icon: <SiAngular /> },
+      { label: 'Node.js', icon: <FaNodeJs /> },
+      { label: 'Express', icon: <SiExpress /> },
+      { label: 'MongoDB', icon: <SiMongodb /> },
+      { label: 'AWS', icon: <FaAws /> }
+    ],
+    description: 'TrialViu is a HIPAA-compliant platform powered by Viuhealth and plays a critical role in connecting people living with autoimmune conditions to relevant clinical trials.',
+    roles: [
+      "Architected and developed the entire TrialViu platform, including a patient onboarding portal, sponsor portal, clinical trial site member portal, and admin dashboard.",
+      "Reused and extended components from the existing ViuHealth WebApps monorepo to maintain consistency and speed up development.",
+      "Configured CI/CD pipelines to automate builds and deployments of the backend services to an AWS EC2 instance.",
+      "Integrated the platform with ViuHealth's backend services to ensure seamless data flow and functionality.",
+      "Digitized the clinical trial workflow for sites, enabling patients to participate more efficiently.",
+      "Implemented performance dashboards for sponsors to monitor site metrics and trial progress."
+    ]
+  },
+
+  {
+    company: 'Viuhealth',
+    title: 'Clinical Trial Matching Algorithm (MAE)',
+    stack: [
+      { label: 'Python', icon: <SiPython /> },
+      { label: 'AWS', icon: <FaAws /> },
+      { label: 'Node.js', icon: <FaNodeJs /> },
+      { label: 'Docker', icon: <SiDocker /> },
+    ],
+    description: 'The team at Viuhealth collaborated with LOKA, to build an extraction and matching algorithm, which leveraged the power of LLMs to automatically assess the likelihood of a specific patient being accepted to a clinical trial based on the trial\'s eligibility criteria.',
+    roles: [
+      "Contributed to the setup and maintenance of the algorithm on the ViuHealth AWS account.",
+      "Made pull requests to the shared codebase while enforcing clear and consistent commit standards.",
+      "Built backend endpoints to enable the algorithm's interaction with ViuHealth's services.",
+      "Implemented a CI/CD pipeline to automate deployment for manual algorithm runs.",
+      "Set up and deployed the algorithm in the production environment.",
+      "Integrated the algorithm into the frontend application, enabling it to be triggered directly from the UI."
+    ]
+  },
+  {
+    company: 'GTBank Ghana',
+    title: 'Myghpay Tickets Infrastructure',
+    stack: [
+      { label: 'React', icon: <SiReact /> },
+      { label: 'ASP.NET', icon: <SiDotnet /> },
+
+    ],
+    description: 'An events ecosystem that enables organizers to create and manage events, while end users can pay through a client-facing web application, embeddable widget, or USSD long code. It also includes an internal administrative portal for managing event organizers.',
+    roles: [
+      "Independently designed and implemented the entire frontend and backend infrastructure.",
+      "Built a client-facing web application for browsing and purchasing event tickets.",
+      "Developed an embeddable payment widget for event organizers to embed in their websites for seamless event checkout experiences.",
+      "Integrated USSD support for event ticket payments via long codes.",
+      "Created an internal administrative portal for managing event organizers.",
+    ]
+  },
+
+  {
+    company: 'GTBank Ghana',
+    title: 'Myghpay Mobile App',
+    stack: [
+      { label: 'React Native', icon: <TbBrandReactNative /> },
+      { label: 'Expo', icon: <SiExpo /> },
+    ],
+    description: 'Myghpay is the safest and most robust online payment and collections platform designed to enable individuals, businesses and institutions to make or receive payments online, from the comfort of their homes, offices and on-the-go.',
+    roles: [
+      "Integrated the Ghana Revenue Authority's E-Levy service into the mobile application.",
+      "Redesigned the payment flow to enhance transparency for customers.",
+      "Implemented quick payment features, allowing users to purchase airtime and send mobile money instantly."
+    ]
+  },
+  {
+    company: 'GTBank Ghana',
+    title: 'Paperless Banking',
+    stack: [
+      { label: 'React Native', icon: <TbBrandReactNative /> },
+      { label: 'Expo', icon: <SiExpo /> },
+    ],
+    description: 'Paperless Banking is a customer-centric initiative that provides safe, secure, and swift transactions. This initiative involves using state-of-the-art user-friendly computers in their branches to facilitate various services such as money transfers, account management, and more, all available at the click of a button.',
+    roles: [
+      "Collaborated on the design and development of a frontend web application for customers to perform banking transactions.",
+      "Contributed to the development of a backend application to store and support processing of customer requests by banking officers.",
+      "Helped build a frontend application for banking officers to manage and process transactions.",
+      "Integrated the customer-facing application with Wacom signature pads to capture digital signatures.",
+      "Worked on a frontend application for internal auditors to review transactions and access banking analytics."
+    ]
+  },
+  {
+    company: 'GTBank Ghana',
+    title: 'Internet Banking Self Onboarding',
+    stack: [
+      { label: 'React', icon: <SiReact /> },
+      { label: 'C#', icon: <TbBrandCSharp /> },
+      { label: '.NET', icon: <SiDotnet /> }
+    ],
+    description: 'The Internet Banking Self Onboarding is an application that allows customers to onboard to the bank\'s internet banking service by themselves.',
+    roles: [
+      "Designed and developed the entire application from scratch, both the frontend and backend.",
+      "Integrated with the core banking system to validate customer details before completing the onboarding process."
+    ]
+  },
+  {
+    company: 'GTBank Ghana',
+    title: 'Myghpay Checkout',
+    stack: [
+      { label: 'React', icon: <SiReact /> },
+      { label: 'C#', icon: <TbBrandCSharp /> },
+      { label: '.NET', icon: <SiDotnet /> },
+    ],
+    description: 'Myghpay Checkout is a payment gateway for merchants to accept payments from their customers.',
+    roles: [
+      "Designed and developed a customer checkout interface for the checkout API.",
+      "Implemented payment status polling to update the customer on the status of their payment.",
+      "Collaborated with the team to build the checkout API.",
+    ]
+  }
+]
+
+
+export { currentStack, experience, projects }
