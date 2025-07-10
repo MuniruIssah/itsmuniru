edef64388f826c21138c1572e23d6d9eb8ff8ef0
[Muniru Issah] <munirissah2092@gmail.com>
2025-07-10 04:12:22 +0000

    misc

--- components/StickyBack.tsx
+++ components/StickyBack.tsx
@@ -6,7 +6,7 @@ import React from 'react'
 const StickyBack = ({type,title}:{type:'post'|'profile',title?:string}) => {
   const router=useRouter()
     return (
-    <div className='flex gap-4 text-white shadow-md px-6 py-5 border-b sticky top-0 backdrop-blur-xl z-[20]'>
+    <div className='flex gap-4 text-white shadow-md px-6 py-5 border-b sticky top-0 bg-black/50 backdrop-blur-xl z-[20]'>
         <button onClick={()=>router.back()}><ArrowLeft/></button>
         <span className='capitalize font-bold'>{type==='post'?'Post':title}</span>
     </div>
